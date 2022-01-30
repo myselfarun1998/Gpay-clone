b7caf73892e255f9c97db90180aad5a8faea2b9f
Arun kumar <72678258+myselfarun1998@users.noreply.github.com>
2022-01-31 00:11:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import './style.css';
+import Bank from './src/Component/Bank';
+import Banksuccess from './src/Component/Banksuccess';
+import Phone from './src/Component/Phone';
+import Phonesuccess from './src/Component/Phonesuccess';
+import Upisuccess from './src/Component/Upisuccess';
+import Phonesuccess from './src/Component/Phonesuccess';
+import Password from './src/Component/Password';
+
+import Upi from './src/Component/Upi';
+
+export default function App() {
+  return (
+    <div>
+     
+      {/* 1<Bank />
+      2<Banksuccess />
+     3 <Phone />
+      4<Phonesuccess />
+     5 <Upi />
+     6 <Upisuccess /> */}
+     <Password />
+    </div>
+  );
+}
